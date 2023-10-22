8637a4ebb1119642c253dde6b34fbf802ea290a0
shivamsinghcse <shivamsinghcse21@gmail.com>
2023-10-23 02:50:03 +0530

    while and do while loop

---
+++ 19.whieanddowhile.js
@@ -0,0 +1,17 @@
+let index =  0; 
+while(index <= 10 ){
+    console.log(`The value of number is ${index}`)
+    index++
+}
+let arr = ["Apple","Dell", "Alienware","Rog"]
+let i =0
+while (i < arr.length){
+    console.log(`My fav laptop is ${arr[i]}`)
+    i++
+}
+//do while loop 
+let j =10 
+do{
+ console.log(`The value is ${j}`)
+}while(j<5)
+//do while loop ek baar kam se kam chalga hi kyun ki yaha pe pahle excution hota hai aur baad mai condition check hota hai
